2a8d6be940fcf34f2f41c8ccf4e1ddd11c1194f0
Baudev <baudev.contact@gmail.com>
2018-05-21 22:16:52 +0200

    Déplace l'installation de zip et unzip du docker dans le script .sh

--- docker_install.sh
+++ docker_install.sh
@@ -9,15 +9,14 @@ set -xe
 apt-get update && apt-get install -y \
         libmcrypt-dev \
     && docker-php-ext-install -j$(nproc) iconv mcrypt
-# apt-get install curl
 apt-get install git -yqq
+apt-get install zip unzip
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 
 # Install pgsql
-# Here you can install any other extension that you need
 apt-get install -y libpq-dev \
     && docker-php-ext-configure pgsql -with-pgsql=/usr/local/pgsql \
     && docker-php-ext-install pdo pdo_pgsql pgsql
