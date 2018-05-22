3d46fa03d287645a65632ff2f08a0c9ed12e495d
Baudev <baudev.contact@gmail.com>
2018-05-23 00:05:04 +0200

    Ajoute l'objet FriendsLinks et commence les relations avec l'objet User

---
+++ Models/Accounts/FriendsLinks.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bdelo
+ * Date: 22/05/2018
+ * Time: 23:14
+ */
+
+namespace ZONNY\Models\Accounts;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="friends_links")
+ */
+class FriendsLinks
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer", name="id")
+     */private $id;
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="friends_links")
+     */
+    private $user;
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="friends_links")
+     */
+    private $friend;
+    /**
+     * @ORM\Column(type="integer", nullable=true, name="facebook_mutual_friends")
+     */
+    private $facebookMutualFriends;
+    /**
+     * @ORM\Column(type="integer", nullable=true, name="facebook_mutual_likes")
+     */
+    private $facebookMutualLikes;
+    /**
+     * @ORM\Column(type="boolean", name="authorization")
+     */
+    private $authorization = false;
+    /**
+     * @ORM\Column(type="datetimetz", name="creation_datetime")
+     */
+    private $creationDatetime;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFriend()
+    {
+        return $this->friend;
+    }
+
+    /**
+     * @param mixed $friend
+     */
+    public function setFriend($friend): void
+    {
+        $this->friend = $friend;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFacebookMutualFriends()
+    {
+        return $this->facebookMutualFriends;
+    }
+
+    /**
+     * @param mixed $facebookMutualFriends
+     */
+    public function setFacebookMutualFriends($facebookMutualFriends): void
+    {
+        $this->facebookMutualFriends = $facebookMutualFriends;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFacebookMutualLikes()
+    {
+        return $this->facebookMutualLikes;
+    }
+
+    /**
+     * @param mixed $facebookMutualLikes
+     */
+    public function setFacebookMutualLikes($facebookMutualLikes): void
+    {
+        $this->facebookMutualLikes = $facebookMutualLikes;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthorization()
+    {
+        return $this->authorization;
+    }
+
+    /**
+     * @param mixed $authorization
+     */
+    public function setAuthorization($authorization): void
+    {
+        $this->authorization = $authorization;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreationDatetime()
+    {
+        return $this->creationDatetime;
+    }
+
+    /**
+     * @param mixed $creationDatetime
+     */
+    public function setCreationDatetime($creationDatetime): void
+    {
+        $this->creationDatetime = $creationDatetime;
+    }
+
+
+
+}
--- Models/Accounts/User.php
+++ Models/Accounts/User.php
@@ -1,30 +1,24 @@
 <?php
 namespace ZONNY\Models\Accounts;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\DBAL\Schema\Column;
 use Doctrine\ORM\Mapping as ORM;
-use ZONNY\Utils\ApiKey;
-use ZONNY\Utils\Application;
-use ZONNY\Utils\Database;
-use ZONNY\Utils\DatetimeISO8601;
-use ZONNY\Utils\ErrorCode;
-use ZONNY\Utils\Functions;
-use ZONNY\Utils\PublicError;
 
 
 /**
  * Class User
  * @package ZONNY\Models\Accounts
  * @ORM\Entity()
- * @ORM\Table()
+ * @ORM\Table(name="user")
  */
 class User implements \JsonSerializable
 {
 
 
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      * @ORM\Column(type="integer", name="id")
      */
     private $id;
@@ -95,7 +89,7 @@ class User implements \JsonSerializable
     /**
      * @ORM\Column(type="boolean", name="facebook_connection")
      */
-    private $facebookConnection;
+    private $facebookConnection = false;
     /**
      * @ORM\Column(type="string", length=255, nullable=true, name="facebook_access_token")
      */
@@ -107,7 +101,7 @@ class User implements \JsonSerializable
     /**
      * @ORM\Column(type="boolean", name="sms_connection")
      */
-    private $smsConnection;
+    private $smsConnection = false;
     /**
      * @ORM\Column(type="string", length=30, nullable=true, name="phone_number")
      */
@@ -115,7 +109,7 @@ class User implements \JsonSerializable
     /**
      * @ORM\Column(type="boolean", name="email_connection")
      */
-    private $emailConnection;
+    private $emailConnection = false;
     /**
      * @ORM\Column(type="string", length=255, nullable=true, name="email")
      */
@@ -127,11 +121,30 @@ class User implements \JsonSerializable
     /**
      * @ORM\Column(type="boolean", name="anonymous_connection")
      */
-    private $anonymousConnection;
+    private $anonymousConnection = false;
     /**
      * @ORM\Column(type="datetimetz", name="creation_datetime")
      */
     private $creationDatetime;
+
+    /**
+     * Foreign keys
+     */
+
+    /**
+     * @ORM\OneToMany(targetEntity=FriendsLinks::class, cascade={"persist", "remove"}, mappedBy="user")
+     */
+    private $friends_links;
+
+    /**
+     * User constructor.
+     * @param $friends_links
+     */
+    public function __construct($friends_links)
+    {
+        $this->friends_links = new ArrayCollection();
+    }
+
 
     /**
      * Specify data which should be serialized to JSON
@@ -144,4 +157,456 @@ class User implements \JsonSerializable
     {
         // TODO: Implement jsonSerialize() method.
     }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProfilePictureUrl()
+    {
+        return $this->profilePictureUrl;
+    }
+
+    /**
+     * @param mixed $profilePictureUrl
+     */
+    public function setProfilePictureUrl($profilePictureUrl): void
+    {
+        $this->profilePictureUrl = $profilePictureUrl;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * @param mixed $pseudo
+     */
+    public function setPseudo($pseudo): void
+    {
+        $this->pseudo = $pseudo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUnavailable()
+    {
+        return $this->unavailable;
+    }
+
+    /**
+     * @param mixed $unavailable
+     */
+    public function setUnavailable($unavailable): void
+    {
+        $this->unavailable = $unavailable;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGcmRegistrationId()
+    {
+        return $this->gcmRegistrationId;
+    }
+
+    /**
+     * @param mixed $gcmRegistrationId
+     */
+    public function setGcmRegistrationId($gcmRegistrationId): void
+    {
+        $this->gcmRegistrationId = $gcmRegistrationId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKeyApp()
+    {
+        return $this->keyApp;
+    }
+
+    /**
+     * @param mixed $keyApp
+     */
+    public function setKeyApp($keyApp): void
+    {
+        $this->keyApp = $keyApp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param mixed $latitude
+     */
+    public function setLatitude($latitude): void
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param mixed $longitude
+     */
+    public function setLongitude($longitude): void
+    {
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastLatitude()
+    {
+        return $this->lastLatitude;
+    }
+
+    /**
+     * @param mixed $lastLatitude
+     */
+    public function setLastLatitude($lastLatitude): void
+    {
+        $this->lastLatitude = $lastLatitude;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastLongitude()
+    {
+        return $this->lastLongitude;
+    }
+
+    /**
+     * @param mixed $lastLongitude
+     */
+    public function setLastLongitude($lastLongitude): void
+    {
+        $this->lastLongitude = $lastLongitude;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDistancePreviousLocation()
+    {
+        return $this->distancePreviousLocation;
+    }
+
+    /**
+     * @param mixed $distancePreviousLocation
+     */
+    public function setDistancePreviousLocation($distancePreviousLocation): void
+    {
+        $this->distancePreviousLocation = $distancePreviousLocation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastLocationCheckUp()
+    {
+        return $this->lastLocationCheckUp;
+    }
+
+    /**
+     * @param mixed $lastLocationCheckUp
+     */
+    public function setLastLocationCheckUp($lastLocationCheckUp): void
+    {
+        $this->lastLocationCheckUp = $lastLocationCheckUp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastInternetCheckUp()
+    {
+        return $this->lastInternetCheckUp;
+    }
+
+    /**
+     * @param mixed $lastInternetCheckUp
+     */
+    public function setLastInternetCheckUp($lastInternetCheckUp): void
+    {
+        $this->lastInternetCheckUp = $lastInternetCheckUp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPlatform()
+    {
+        return $this->platform;
+    }
+
+    /**
+     * @param mixed $platform
+     */
+    public function setPlatform($platform): void
+    {
+        $this->platform = $platform;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFacebookConnection()
+    {
+        return $this->facebookConnection;
+    }
+
+    /**
+     * @param mixed $facebookConnection
+     */
+    public function setFacebookConnection($facebookConnection): void
+    {
+        $this->facebookConnection = $facebookConnection;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFacebookAccessToken()
+    {
+        return $this->facebookAccessToken;
+    }
+
+    /**
+     * @param mixed $facebookAccessToken
+     */
+    public function setFacebookAccessToken($facebookAccessToken): void
+    {
+        $this->facebookAccessToken = $facebookAccessToken;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFacebookUserId()
+    {
+        return $this->facebookUserId;
+    }
+
+    /**
+     * @param mixed $facebookUserId
+     */
+    public function setFacebookUserId($facebookUserId): void
+    {
+        $this->facebookUserId = $facebookUserId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSmsConnection()
+    {
+        return $this->smsConnection;
+    }
+
+    /**
+     * @param mixed $smsConnection
+     */
+    public function setSmsConnection($smsConnection): void
+    {
+        $this->smsConnection = $smsConnection;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param mixed $phoneNumber
+     */
+    public function setPhoneNumber($phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmailConnection()
+    {
+        return $this->emailConnection;
+    }
+
+    /**
+     * @param mixed $emailConnection
+     */
+    public function setEmailConnection($emailConnection): void
+    {
+        $this->emailConnection = $emailConnection;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnonymousConnection()
+    {
+        return $this->anonymousConnection;
+    }
+
+    /**
+     * @param mixed $anonymousConnection
+     */
+    public function setAnonymousConnection($anonymousConnection): void
+    {
+        $this->anonymousConnection = $anonymousConnection;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreationDatetime()
+    {
+        return $this->creationDatetime;
+    }
+
+    /**
+     * @param mixed $creationDatetime
+     */
+    public function setCreationDatetime($creationDatetime): void
+    {
+        $this->creationDatetime = $creationDatetime;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFriendsLinks()
+    {
+        return $this->friends_links;
+    }
+
+    /**
+     * @param FriendsLinks $friends_link
+     */
+    public function addFriendsLink(FriendsLinks $friends_link): void
+    {
+        $this->friends_links->add($friends_link);
+        $friends_link->setUserId($this);
+    }
+
+
+
 }
