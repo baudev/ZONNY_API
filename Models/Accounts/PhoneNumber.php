d25c9faa6c978fb5445cbdc5788d38d312a80648
Baudev <baudev.contact@gmail.com>
2018-05-23 13:41:42 +0200

    Ajoute la classe PhoneNumber

---
+++ Models/Accounts/PhoneNumber.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 23/05/2018
+ * Time: 13:40
+ */
+
+namespace ZONNY\Models\Accounts;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class PhoneNumber
+ * @package ZONNY\Models\Accounts
+ * @ORM\Entity
+ * @ORM\Table(name="phone_numbers")
+ */
+class PhoneNumber
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $userId;
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $phoneNumbers;
+    /**
+     * @ORM\Column(type="datetimetz")
+     */
+    private $creationDatetime;
+
+}
