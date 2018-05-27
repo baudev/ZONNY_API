344f6238c7427d8aa2621c1b170f6d715acb3150
Baudev <baudev.contact@gmail.com>
2018-05-27 21:09:37 +0200

    Ajoute LogRepository

---
+++ Repositories/Helpers/LogRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+use ZONNY\Models\Helpers\Log;
+use ZONNY\Utils\Database;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 21:08
+ */
+
+class LogRepository
+{
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(Log::class);
+    }
+
+
+}
