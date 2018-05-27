8bdbd9001b9703e29cf0bdbd71b3c16e76de10b6
Baudev <baudev.contact@gmail.com>
2018-05-27 20:58:52 +0200

    Ajoute le PhoneNumberRepository

---
+++ Repositories/Account/PhoneNumberRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+use ZONNY\Models\Account\PhoneNumber;
+use ZONNY\Utils\Database;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 20:58
+ */
+
+class PhoneNumberRepository
+{
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(PhoneNumber::class);
+    }
+
+}
