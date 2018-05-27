b34752df2cad8955e1503d962337ed64c5b21370
Baudev <baudev.contact@gmail.com>
2018-05-27 20:56:56 +0200

    Ajoute le FriendsLinkRepository

---
+++ Repositories/Account/FriendsLinkRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+use ZONNY\Models\Account\FriendsLink;
+use ZONNY\Utils\Database;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 20:55
+ */
+
+class FriendsLinkRepository
+{
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(FriendsLink::class);
+    }
+}
