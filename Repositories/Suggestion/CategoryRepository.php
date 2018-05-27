f74da233ea05733b771a2e72e1e28cc3ab4efc4f
Baudev <baudev.contact@gmail.com>
2018-05-27 21:10:13 +0200

    Ajoute CategoryRepository

---
+++ Repositories/Suggestion/CategoryRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+use ZONNY\Models\Suggestion\Category;
+use ZONNY\Utils\Database;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 21:09
+ */
+
+class CategoryRepository
+{
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(Category::class);
+    }
+
+
+}
