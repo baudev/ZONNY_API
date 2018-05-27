4802a93d3975b2666e2e921f29fab06f1f7815c4
Baudev <baudev.contact@gmail.com>
2018-05-27 21:11:14 +0200

    Ajoute SuggestionRepository

---
+++ Repositories/Suggestion/SuggestionRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+use ZONNY\Models\Suggestion\Suggestion;
+use ZONNY\Utils\Database;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 21:10
+ */
+
+class SuggestionRepository
+{
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(Suggestion::class);
+    }
+
+}
