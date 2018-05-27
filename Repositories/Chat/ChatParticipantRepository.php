69360877c04baa87240d441b5b7bb35b3bac8dbb
Baudev <baudev.contact@gmail.com>
2018-05-27 21:04:46 +0200

    Ajoute les Repositories pour ChatMessae, ChatParticipant, PenfingOperation et State

---
+++ Repositories/Chat/ChatParticipantRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+use ZONNY\Models\Chat\ChatParticipant;
+use ZONNY\Utils\Database;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 21:02
+ */
+
+class ChatParticipantRepository
+{
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(ChatParticipant::class);
+    }
+
+}
---
+++ Repositories/Chat/PendingOperationRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+use ZONNY\Models\Chat\PendingOperation;
+use ZONNY\Utils\Database;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 21:02
+ */
+
+class PendingOperationRepository
+{
+
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(PendingOperation::class);
+    }
+
+}
