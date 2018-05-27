2348da377cfaed045696d2b0ee4f2016618d8fbf
Baudev <baudev.contact@gmail.com>
2018-05-27 21:08:29 +0200

    Ajoute ErrorRepository

---
+++ Repositories/Helpers/ErrorRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+use ZONNY\Utils\Database;
+use \ZONNY\Models\Helpers\Error;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 21:07
+ */
+
+class ErrorRepository
+{
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(Error::class);
+    }
+
+}
