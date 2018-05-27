6c5973cab128d791d618f2ad752ff1961a213b35
Baudev <baudev.contact@gmail.com>
2018-05-27 21:07:15 +0200

    Ajoute EventRequestRepository

---
+++ Repositories/Event/EventRequestRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+use ZONNY\Models\Event\EventRequest;
+use ZONNY\Utils\Database;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 21:06
+ */
+
+class EventRequestRepository
+{
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(EventRequest::class);
+    }
+
+
+}
