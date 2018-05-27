4abe930003155cf47321a1ff51517daa89aebe9a
Baudev <baudev.contact@gmail.com>
2018-05-27 21:10:42 +0200

    Ajoute DayTimeRepository

---
+++ Repositories/Suggestion/DayTimeRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+use ZONNY\Models\Suggestion\DayTime;
+use ZONNY\Utils\Database;
+
+/**
+ * Created by PhpStorm.
+ * User: Baudev
+ * Date: 27/05/2018
+ * Time: 21:10
+ */
+
+class DayTimeRepository
+{
+
+    /**
+     * @return \Doctrine\Common\Persistence\ObjectRepository|\Doctrine\ORM\EntityRepository
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public static function getRepository(){
+        return Database::getEntityManager()->getRepository(DayTime::class);
+    }
+
+}
