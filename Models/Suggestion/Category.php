92a107fbd1c64ca507dd19a053415eca34c3f7d4
Baudev <baudev.contact@gmail.com>
2018-06-03 15:02:12 +0200

    Ajoute les getters et setters manquants de la classe Category

--- Models/Suggestion/Category.php
+++ Models/Suggestion/Category.php
@@ -34,6 +34,53 @@ class Category
      */
     private $creationDatetime;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreationDatetime()
+    {
+        return $this->creationDatetime;
+    }
+
+    /**
+     * @param mixed $creationDatetime
+     */
+    public function setCreationDatetime($creationDatetime): void
+    {
+        $this->creationDatetime = $creationDatetime;
+    }
 
     /**
      * Foreign keys
