9cad7801ab398ac9c2cda0d774af912ddf8140e2
Baudev <baudev.contact@gmail.com>
2018-06-03 22:22:08 +0200

    Documente les méthodes randomFloat et replace_key de la classe Function

--- Utils/Functions.php
+++ Utils/Functions.php
@@ -5,12 +5,25 @@ namespace ZONNY\Utils;
 class Functions{
 
 
+    /**
+     * Génère un nombre aléatoire
+     * @param int $min Borne inférieure
+     * @param int $max Borne supérieure
+     * @return float|int
+     */
     public static function randomFloat($min = -1, $max = 1)
     {
         return $min + mt_rand() / mt_getrandmax() * ($max - $min);
     }
 
-    public static function replace_key($arr, $oldkey, $newkey) {
+    /**
+     * Remplace dans un tableau donnée, la clé $oldkey par $newkey
+     * @param $arr Tableau dans lequel la clé est remplacée
+     * @param $oldkey Clé à remplacer
+     * @param $newkey Clé par laquelle remplacer l'ancienne
+     * @return array
+     */
+    public static function replace_key($arr, $oldkey, $newkey) : array {
         $arr[$newkey] = $arr[$oldkey];
         unset($arr[$oldkey]);
         return $arr;
